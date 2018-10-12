02120c9aa4c6565ee1e4817ef0b347fe081599ac
vadim <sorax105@mail.ru>
2018-10-12 22:31:38 +0300

    dz-2

--- lesson-5 dz-2/js/main.js
+++ lesson-5 dz-2/js/main.js
@@ -1,22 +1,25 @@
 let start = document.getElementById('start'),
-  daybudgetValue = document.getElementsByClassName('daybudget-value'),
-  levelValue = document.getElementsByClassName('level-value'),
-  expensesValue = document.getElementsByClassName('expenses-value'),
-  optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value'),
-  incomeValue = document.getElementsByClassName('income-value'),
-  monthsavingsValue = document.getElementsByClassName('monthsavings-value'),
-  yearsavingsValue = document.getElementsByClassName('yearsavings-value'),
-  expensesItem = document.getElementsByClassName('expenses-item'),
+  daybudgetValue = document.getElementsByClassName('daybudget-value')[0],
+  levelValue = document.getElementsByClassName('level-value')[0],
+  expensesValue = document.getElementsByClassName('expenses-value')[0],
+  optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+  incomeValue = document.getElementsByClassName('income-value')[0],
+  monthsavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+  yearsavingsValue = document.getElementsByClassName('yearsavings-value')[0],
+  expensesItem = document.getElementsByClassName('expenses-item')[0],
   taG = document.getElementsByTagName('button'),
   lexpensesBtn = taG[1],
   countBudgetBtn = document.getElementsByTagName('button')[2],
-  optionalexpensesItem = document.querySelectorAll('optionalexpenses-item'),
-  savings = document.querySelector('.savings'),
+  expensesItemBtn = document.getElementsByTagName('button')[0],
+  optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+  savings = document.querySelector('#savings'),
   chooseSum = document.querySelector('.choose-sum'),
   choosePercent = document.querySelector('.choose-percent'),
   yearValue = document.querySelector('.year-value'),
-  cmonth = document.querySelector('.month'),
+  month = document.querySelector('.month-value'),
+  chooseIncome = document.querySelector('.choose-income'),
   dayValue = document.querySelector('.day-value');
 
-console.log(lexpensesBtn);
-console.log(start);
+console.log(optionalexpensesItem);
+console.log(month);
+console.log(savings);
