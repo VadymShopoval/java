b85350d7b3565588d30642a52358c603f209ed61
vadim <sorax105@mail.ru>
2018-10-19 21:26:29 +0300

    leson-7

--- lesson-7/js.js
+++ lesson-7/js.js
@@ -1,11 +1,15 @@
-let date = new Date(),
-    h = date.getHours(),
-    s = date.getSeconds(),
-    m = date.getMinutes();
+function time(){
+  let date = new Date(),
+    h = ('0' + date.getHours()).slice(-2),
+    s = ('0' + date.getSeconds()).slice(-2),
+    m = ('0' + date.getMinutes()).slice(-2);
+  //let a = ('0' + s).slice(-2);
+  //console.log(h + ':' + m + ':' + s);
+  document.getElementById('time').innerHTML = h + ':' + m + ':' + s;
+  
+};
+setInterval('time()', 1000);
 
-let a = '0' + s.slice(-2);
-
-console.log(h + ':'+ m +':'+ s);
 /* function twoDigits(num) {
   return ('0' + num).slice(-2);
 }
