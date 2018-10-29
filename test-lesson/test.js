9b9aa2de5b79ff21398c5c9e50e0cb6ebab115e3
vadim <sorax105@mail.ru>
2018-10-29 18:58:43 +0300

    test

---
+++ test-lesson/test.js
@@ -0,0 +1,29 @@
+/* function sayName(name) {
+  let  message ="My name is" + name;
+  return message;
+}
+let arr = [5,-3,6,-5,0,-7,8,10];
+let result = arr.reduce(function (sum, elem) {
+  return sum + elem;
+})
+
+let assert = require('chai').assert;
+
+describe('sayName', function () {
+  it('получаем фразу с новым именем', function () {
+    assert.typeOf(sayName('vadim'), 'string');
+  });
+});
+describe('arr', function () {
+  it('получаем сумму чисел массива', function () {
+    // result=== 13
+    assert.equal(result, 13);
+  });
+}); */
+//html
+describe('Таймер', function () {
+  it('Возращает функция обьект?', function () {
+    
+    assert.typeOf(getTimeRemaining(), 'object');
+  });
+});
